d805c813807b5d7e552aafe312f31204907d2080
emrys66 <mikesteinfield@gmail.com>
2025-05-13 22:42:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ class_names = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer',
                'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
 # Streamlit UI
-st.title("Image Classifier using CIFAR 10 Dataset")
-st.markdown("Upload a 32x32 color image to check the classification in within the CIFAR 10 Dataset")
+st.title("Image Classifier withing CIFAR 10 Dataset")
+st.markdown("Upload a 32x32 color image to check the classification base on the CIFAR 10 Dataset")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
